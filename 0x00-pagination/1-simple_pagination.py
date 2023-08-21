d5a29a94da94155d36705bd9740c501ca0950e46
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-21 17:57:38 +0000

    the correct annotaion

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Simple helper function"""
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """returns a tuple"""
     sIdx = (page - 1) * page_size
     eIdx = sIdx + page_size
