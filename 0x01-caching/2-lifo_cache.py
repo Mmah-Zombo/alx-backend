4f8edf38c9bc8cd7a6a8306c14c94b8727cbd375
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-23 12:57:37 +0000

    implements the LIFO cache replacement policy

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""contains a class that implements LIFO caching"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LIFOCache(BaseCaching):
+    """a cahcing system that implements the first in
+    first out cache replacement policy
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """adds data to the cache"""
+
+        if (key is None or item is None):
+            return
+
+        self.cache_data[key] = item
+
+        if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+            ckey, _ = self.cache_data.popitem()
+            print(f"DISCARD:", ckey)
+
+    def get(self, key):
+        """gets the cache data for a specific key"""
+        return (self.cache_data.get(key))
