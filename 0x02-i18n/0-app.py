f5eb448f7ab70034f19450c15bc137b89a28432e
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-29 17:23:07 +0100

    task0 again and again

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """this is the main app"""
-from flask import Flask, render_template, Response
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index() -> Response:
+def index() -> str:
     """renders a html page"""
     return render_template('0-index.html')
 
