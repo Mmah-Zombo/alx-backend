1f791a868f07c2d984d7dc663f09c917cec2c1be
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-29 18:52:07 +0100

    task4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""this is the main app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config():
+    """a class that configures the required languges"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index() -> str:
+    """renders a html page"""
+    return render_template('0-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """sets the defualt language for a user session"""
+    if 'locale' in request.args and (request.args['locale']
+                                     in app.config['LANGUAGES']):
+        return request.args['locale']
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
