12378a7be869201c236e4d0d1d31f5bfea985ba3
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-29 17:19:29 +0100

    task0 - type annotation and new line at the end of file

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """this is the main app"""
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def index() -> Response:
+    """renders a html page"""
     return render_template('0-index.html')
