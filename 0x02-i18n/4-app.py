673c2be6d04d375a73b757ad384b70c00dde05a1
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-31 14:15:50 +0100

    this is it

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -27,7 +27,7 @@ def get_locale() -> str:
     """sets the defualt language for a user session"""
     if 'locale' in request.args and (request.args['locale']
                                      in app.config['LANGUAGES']):
-        return request.args['locale']
+        return str(request.args['locale'])
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
