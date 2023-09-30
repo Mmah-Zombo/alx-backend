3f7c2082266c6d51bdc03d3f874b290d3056ed73
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-30 03:00:49 +0000

    t5

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -9,3 +9,12 @@ client.on('error', (err) => {
 client.on('connect', () => {
     console.log('Redis client connected to the server');
 });
+
+client.on('message', (channel, message) => {
+  console.log(`Message received on channel ${channel}: ${message}`);
+  
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe('holberton school channel');
+    client.quit();
+  }
+});
